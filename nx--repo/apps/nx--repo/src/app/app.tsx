e4b8605c417cd697a4d9605d2c58a0f7c113c1d1
venkatk-git <venkatkumar.profiles@gmail.com>
2024-12-02 19:47:18 +0530

    Monorepo-Test: Initial commit

---
+++ nx--repo/apps/nx--repo/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="nx--repo" />
+    </div>
+  );
+}
+
+export default App;
---
+++ nx-standalone--repo/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="nx-standalone--repo" />
+    </div>
+  );
+}
+
+export default App;
